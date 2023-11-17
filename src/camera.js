3d4583b372edb0aae0e75c02dcb956c6998107de
alexmtz2002 <52327548+alexmtz2002@users.noreply.github.com>
2023-11-17 13:30:16 -0600

    Update camera.js
    Handle Permission Denied. Add a check for the NotAllowedError and provide an error message to the user. Check for Browser Support: Ensure that the necessary APIs are supported by the user's browser. This feature can be used for detection before attempting to access media devices. Clean Up Stream on Page Exit: If the video stream is still active when the user leaves the page, stop the stream to release the camera resource.

--- src/camera.js
+++ src/camera.js
@@ -1,9 +1,15 @@
+// Get the video element by ID
 const video = document.getElementById('camera-feed');
+
 // Get the camera selection menu
 const select = document.getElementById('camera-select');
 
+// Add event listener to handle camera selection
 select.addEventListener('click', event => {
+  // Get the selected device ID from the menu
   const deviceId = select.value;
+
+  // Set up constraints with the selected device ID
   const constraints = {
     video: {
       deviceId: {
@@ -11,46 +17,62 @@ select.addEventListener('click', event => {
       },
     },
   };
-  navigator.mediaDevices
-  .getUserMedia(constraints)
-  .then(stream => {
-    video.srcObject = stream;
-  })
-  .catch(error => {
-    console.error('Error accessing media devices.', error);
-  });
+
+  // Request access to the camera with the specified constraints
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+      // Set the video element's source to the camera stream
+      video.srcObject = stream;
+    })
+    .catch(error => {
+      console.error('Error accessing media devices.', error);
+    });
 });
 
-// Ask for permission to use the camera
+// Ask for permission to use the camera initially
 navigator.mediaDevices.getUserMedia({ video: true })
   .then(stream => {
-      // Set the video element's source to the camera stream
-        video.srcObject = stream;
+    // Set the video element's source to the camera stream
+    video.srcObject = stream;
 
-        // Get a list of available cameras
-        navigator.mediaDevices.enumerateDevices()
-          .then(devices => {
-            // Filter the list to only include video input devices
-            const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    // Get a list of available cameras
+    navigator.mediaDevices.enumerateDevices()
+      .then(devices => {
+        // Filter the list to only include video input devices
+        const videoDevices = devices.filter(device => device.kind === 'videoinput');
 
-            // Create a list of options for the camera selection menu
-            const options = videoDevices.map(device => {
-              return `<option value="${device.deviceId}">${device.label}</option>`;
-            });
+        // Create a list of options for the camera selection menu
+        const options = videoDevices.map(device => {
+          return `<option value="${device.deviceId}">${device.label}</option>`;
+        });
 
-            // Add the options to the camera selection menu
-            select.innerHTML = options.join('');
-          })
-          .catch(error => {
-            console.error('Error fetching video devices:', error);
-          });
+        // Add the options to the camera selection menu
+        select.innerHTML = options.join('');
       })
       .catch(error => {
-        console.error('Error accessing camera:', error);
+        console.error('Error fetching video devices:', error);
       });
+  })
+  .catch(error => {
+    // Handle permission denied scenarios
+    if (error.name === 'NotAllowedError') {
+      console.error('Permission to access camera was denied by the user.');
+    } else {
+      console.error('Error accessing camera:', error);
+    }
+  });
 
-    // Add event listener to back button
-    const backButton = document.getElementById('back-button');
-    backButton.addEventListener('click', () => {
-      window.location.href = 'index.html';
-    });
+// Add event listener to the back button
+const backButton = document.getElementById('back-button');
+backButton.addEventListener('click', () => {
+  // Navigate back to the index.html page
+  window.location.href = 'index.html';
+});
+
+// Clean up the video stream on page exit
+window.addEventListener('beforeunload', () => {
+  if (video.srcObject) {
+    const tracks = video.srcObject.getTracks();
+    tracks.forEach(track => track.stop());
+  }
+});
